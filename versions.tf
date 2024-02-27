afe785808127b8475fda97f5dbe783b3ec79a223
Bharath KKB <bharathkrishnakb@gmail.com>
2024-02-27 12:41:37 -0600

    chore: update meta semver (#12)

--- versions.tf
+++ versions.tf
@@ -23,6 +23,6 @@ terraform {
     }
   }
   provider_meta "google" {
-    module_name = "blueprints/terraform/terraform-google-netapp-volumes/v0.1"
+    module_name = "blueprints/terraform/terraform-google-netapp-volumes/v0.1.0"
   }
 }
